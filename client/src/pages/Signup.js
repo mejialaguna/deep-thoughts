54aa2fceebf079812fd7d21476dfe15b1b272497
Jose Mejia <mejialaguna@yahoo.com>
2021-11-05 16:44:49 -0400

    serverSide-Auth

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../utils/mutations";
 
 const Signup = () => {
   const [formState, setFormState] = useState({
@@ -6,6 +8,9 @@ const Signup = () => {
     email: "",
     password: "",
   });
+
+  const [addUser, { error }] = useMutation(ADD_USER);
+  // he useMutation() Hook creates and prepares a JavaScript function that wraps around our mutation code and returns it to us. In our case, it returns in the form of the addUser function that's returned. We also get the ability to check for errors.
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -18,9 +23,23 @@ const Signup = () => {
   };
 
   // submit form
+  // submit form (notice the async!)
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+    
+    // We use the try...catch block functionality here, as it is especially useful with asynchronous code such as Promises. This way, we can use async/await instead of .then() and .catch() method-chaining while still being able to handle any errors that may occur.
+    try {
+      // execute addUser mutation and pass in variable data from form
+      const { data } = await addUser({
+        variables: { ...formState },
+      });
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
   };
+
+  
 
   return (
     <main className="flex-row justify-center mb-4">
@@ -60,6 +79,9 @@ const Signup = () => {
                 Submit
               </button>
             </form>
+
+            {error && <div>Sign up failed</div>}
+            
           </div>
         </div>
       </div>
