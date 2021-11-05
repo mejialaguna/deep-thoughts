820d0b1873dd7d954f0e2c9c3c6006b985524292
Jose Mejia <mejialaguna@yahoo.com>
2021-11-04 22:30:58 -0400

    react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   ApolloProvider,
   //is a special type of React component that we'll use to provide data to all of the other components.
@@ -9,12 +9,15 @@ import {
   createHttpLink,
   //allows us to control how the Apollo Client makes a request. Think of it like middleware for the outbound network requests.
 } from "@apollo/client";
-
-import Header from './components/Header';
-import Footer from './components/Footer';
-
-import Home from './pages/Home';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+import SingleThought from "./pages/SingleThought";
+import Home from "./pages/Home";
+import NoMatch from "./pages/NoMatch";
 
 // With the preceding code, we first establish a new link to the GraphQL server at its /graphql endpoint with createHttpLink(). We could pass many other options and configuration settings into this function.
 const httpLink = createHttpLink({
@@ -29,18 +32,30 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-
 function App() {
   return (
     // Note how we wrap the entire returning JSX code with <ApolloProvider>. Because we're passing the client variable in as the value for the client prop in the provider, everything between the JSX tags will eventually have access to the server's API data through the client we set up.
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile" component={Profile} />
+              <Route exact path="/thought" component={SingleThought} />
+              <Route component={NoMatch} />
+              {/* e've wrapped all of the Route components in a Switch component and
+              included one more Route at the end to render the NoMatch
+              component. If the route doesn't match any of the preceding paths
+              (e.g., /about), then users will see the 404 message. */}
+            </Switch>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
