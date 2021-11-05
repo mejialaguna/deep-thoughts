81df3b25fae66d5c47008f2a3bf0ec5121304c4f
Jose Mejia <mejialaguna@yahoo.com>
2021-11-05 11:59:37 -0400

    profile/page

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,22 +1,42 @@
 import React from 'react';
 import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { QUERY_THOUGHT } from "../utils/queries";
+import ReactionList from "../components/ReactionList";
 
 const SingleThought = props => {
   const { id: thoughtId } = useParams();
   console.log(thoughtId);
+
+  const { loading, data } = useQuery(QUERY_THOUGHT, {
+    variables: { id: thoughtId },
+    // This is how you can pass variables to queries that need them. The id property on the variables object will become the $id parameter in the GraphQL query.
+  });
+
+  const thought = data?.thought || {};
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <div className="card mb-3">
         <p className="card-header">
           <span style={{ fontWeight: 700 }} className="text-light">
-            Username
-          </span>{' '}
-          thought on createdAt
+            {thought.username}
+          </span>{" "}
+          thought on {thought.createdAt}
         </p>
         <div className="card-body">
-          <p>Thought Text</p>
+          <p>{thought.thoughtText}</p>
         </div>
       </div>
+      {thought.reactionCount > 0 && (
+        <ReactionList reactions={thought.reactions} />
+      )}
+      {/* We combined this with a thought.reactionCount > 0 expression to prevent
+      rendering the reactions if the array is empty. */}
     </div>
   );
 };
